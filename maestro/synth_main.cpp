97563d340d4a9bf131046ea0911d3969216a3888
Jungho Bang <me@bangtoven.com>
2017-11-28 00:58:57 -0500

    changed the example

--- maestro/synth_main.cpp
+++ maestro/synth_main.cpp
@@ -32,15 +32,23 @@ int main(int argc, char* argv[]) {
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    for (MSInstNode *i : s.getInstruments()) {
-        i->start(0.5);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    for (int i=0; i<count; i++) {
+        MSInstNode *inst = s.getInstruments()[i];
+        inst->start(0.5);
+        if (i==0 || i==2) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
     }
+    
+//    for (MSInstNode *i : s.getInstruments()) {
+//        i->start(0.5);
+//        std::this_thread::sleep_for(std::chrono::seconds(1));
+//    }
     
 //    for (MSInstNode *i : s.getInstruments())
 //        i->setGain(1.0);
     
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     
     for (MSInstNode *i : s.getInstruments()) {
         i->release();
